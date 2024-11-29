defebce120c6e1e3f0d32f707f8fe7582cc05aae
Paolo <p.ferri1986@gmail.com>
2024-11-29 18:51:15 +0800

    main loop and game class

---
+++ game.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Game:
+    def __init__(self) -> None:
+        pass
+
+    def Update(self) -> None:
+        pass
+
+    def Render(self, screen) -> None:
+        pass
+
+    def OnEvent(self, event: pygame.event.Event):
+        pass
---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from game import *
+
+pygame.init()
+
+ScreenWidth = 800
+ScreenHeight = 600
+
+m_screen = pygame.display.set_mode((ScreenWidth, ScreenHeight))
+m_isRunning = True
+
+m_game = Game()
+while m_isRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            m_isRunning = False
+        else:
+            m_game.OnEvent(event)
+
+    m_game.Update()
+
+    m_screen.fill((0,0,0))
+    m_game.Render(m_screen)
+    pygame.display.flip()
+
+pygame.quit()
+
+
